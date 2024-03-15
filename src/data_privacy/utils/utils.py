9ac03f9e8cdad90c0f10e46ce1c7354ca3da9df8
Vikas Lakka <vikaslakkacs@gmail.com>
2024-03-15 18:18:19 +0530

    utils

---
+++ src/data_privacy/utils/utils.py
@@ -0,0 +1,17 @@
+import os
+from langchain_core.documents import Document
+
+###loading documents
+def load_document(path) -> list:
+    """takes the path of the file and generates doc strings.
+
+    Args:
+        path (string): Generate document from text data(currently)
+    """
+    with open(path, 'r') as doc_text:
+        document=[Document(page_content= doc_text.read())]
+        return document
+
+
+
+print(load_document('/Users/vikaslakka/Desktop/FSDS/GenAI/poc/data_privacy/data_privacy/cases/theft_case.txt'))
