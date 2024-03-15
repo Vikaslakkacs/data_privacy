f09998a248f583f1df7e11c4d9af5e390cf6ec04
Vikas Lakka <vikaslakkacs@gmail.com>
2024-03-15 22:28:23 +0530

    added src values

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup (
     author='vikaslakkacs@gmail.com',
     author_email='vikaslakkacs@gmail.com',
     install_requires= ['openai', 'langchain', 'streamlit', 'python-dotenv', 'PyPDF2'],
-    packages= find_packages()
+    packages= find_packages(where="src"),
+    package_dir={"":"src"}
 )
